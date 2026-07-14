6b1922d500646f67e169f6de1afdb1612f6e53e7
faisal-shohag <faisalshohagprog@gmail.com>
2026-07-14 22:44:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const dns = require("node:dns");
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
+
+const express = require("express");
+const dontenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+dontenv.config();
+
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.CLIENT_URL],
+  }),
+);
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("tech-bazaar");
+    const subscriptionCollection = db.collection("subscription");
+    const userCollection = db.collection("user");
+
+    app.post("/subscription", async (req, res) => {
+      const { user, session_id } = req.body;
+
+      const isExistSession = await subscriptionCollection.findOne({session_id})
+      if(isExistSession){
+        return res.status(400).send({message: "Session already exist"})
+      }
+
+      const subs_result = await subscriptionCollection.insertOne({
+        userId: new ObjectId(user.id),
+        session_id,
+      });
+
+      const user_result = await userCollection.updateOne(
+        { _id: new ObjectId(user.id) },
+        { $set: { plan: "pro" } },
+      );
+
+
+      
+      res.send({ subs_result, user_result });
+
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
