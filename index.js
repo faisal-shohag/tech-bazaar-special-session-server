950f57bf6502319d2b919866b9a10ac848c1cd62
faisal-shohag <faisalshohagprog@gmail.com>
2026-07-19 23:20:14 +0600

    part-2

--- index.js
+++ index.js
@@ -33,14 +33,18 @@ async function run() {
     await client.connect();
     const db = client.db("tech-bazaar");
     const subscriptionCollection = db.collection("subscription");
+    const paymentCollection = db.collection("payment");
     const userCollection = db.collection("user");
+    const productCollection = db.collection("products");
 
     app.post("/subscription", async (req, res) => {
       const { user, session_id } = req.body;
 
-      const isExistSession = await subscriptionCollection.findOne({session_id})
-      if(isExistSession){
-        return res.status(400).send({message: "Session already exist"})
+      const isExistSession = await subscriptionCollection.findOne({
+        session_id,
+      });
+      if (isExistSession) {
+        return res.status(400).send({ message: "Session already exist" });
       }
 
       const subs_result = await subscriptionCollection.insertOne({
@@ -53,10 +57,48 @@ async function run() {
         { $set: { plan: "pro" } },
       );
 
-
-      
       res.send({ subs_result, user_result });
+    });
 
+    app.post("/payment", async (req, res) => {
+      const { price, userId, title, productId, session_id } = req.body;
+
+      const isExistSession = await paymentCollection.findOne({ session_id });
+      if (isExistSession) {
+        return res.status(400).send({ message: "Session already exist" });
+      }
+
+      const pay_result = await paymentCollection.insertOne({
+        userId,
+        session_id,
+        price: Number(price),
+        title,
+        productId,
+      });
+
+      res.send({ pay_result });
+    });
+
+    app.post("/product", async (req, res) => {
+      const data = req.body;
+      const result = await productCollection.insertOne({
+        ...data,
+        price: Number(data.price),
+        quantity: Number(data.quantity),
+      });
+      res.send(result);
+    });
+
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+
+      res.send(result);
+    });
+
+    app.get("/product/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await productCollection.findOne({ _id: id });
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
